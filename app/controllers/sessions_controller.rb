8ecb7dd627cabd4555e970440b51cbd6982d9244
le.manh.dat <le.manh.dat@sun-asterisk.com>
2022-01-14 00:50:44 +0700

    Test #3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,6 @@ class SessionsController < Devise::SessionsController
   respond_to :json
 
   private
-
   def respond_with _resource, _opts = {}
     render json: {
       access_token: current_token
